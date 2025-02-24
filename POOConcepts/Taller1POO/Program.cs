df402c5f607cfb9df00cb76058f78cca71d5ecaf
SergioVF89 <fonnegrasergio@gmail.com>
2025-02-24 18:09:10 -0500

    Clase Time

---
+++ POOConcepts/Taller1POO/Program.cs
@@ -0,0 +1,25 @@
+﻿using Taller1POO;
+
+try
+{
+    var t1 = new Time();
+    var t2 = new Time(14);
+    var t3 = new Time(9, 34);
+    var t4 = new Time(19, 45, 56);
+    var t5 = new Time(23, 3, 45, 678);
+
+    var times = new List<Time> { t1, t2, t3, t4, t5 };
+
+    foreach (var time in times)
+    {
+        Console.WriteLine($"Time: {time}");
+        Console.WriteLine($"\tMilliseconds: {time.ToMilliseconds(),15:N0}");
+        Console.WriteLine($"\tSeconds     : {time.ToSeconds(),15:N0}");
+        Console.WriteLine($"\tMinutes     : {time.ToMinutes(),15:N0}");
+    }
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex);
+}
---
+++ POOConcepts/Taller1POO/Time.cs
@@ -0,0 +1,71 @@
+﻿namespace Taller1POO;
+
+class Time
+{
+    private int _hour;
+    private int _minute;
+    private int _seconds;
+    private int _milliseconds;
+    public  Time()
+    {
+        _hour = 0;
+        _minute = 0;
+        _seconds = 0;
+        _milliseconds = 0;
+    }
+
+    public Time (int hour) { _hour = ValidateHour(hour); _minute = 0; _seconds = 0; _milliseconds = 0; }
+    public Time(int hour, int minute) { _hour = ValidateHour(hour); _minute = ValidateMinute(minute); _seconds = 0; _milliseconds = 0; }
+    public Time(int hour, int minute, int seconds) { _hour = ValidateHour(hour); _minute = ValidateMinute(minute); _seconds = ValidateSeconds(seconds); _milliseconds = 0; }
+    public Time(int hour, int minute, int seconds, int milliseconds) { _hour = ValidateHour(hour); _minute = ValidateMinute(minute); _seconds = ValidateSeconds(seconds); _milliseconds = ValidateMilliseconds(milliseconds); }
+
+    public int Hour
+    {
+        get => _hour;
+        set => _hour = value;
+    }
+    public int Minute { get => _minute; set => _minute = value; }
+    public int Seconds { get => _seconds; set => _seconds = value; }
+    public int Milliseconds { get => _milliseconds; set => _milliseconds = value; }
+
+    // public override string ToString() => $"{_hour:00}:{_minute:00}:{_seconds:00}.{_milliseconds:000}";
+
+    public override string ToString()
+    {
+        DateTime time = new DateTime(1, 1, 1, _hour, _minute, _seconds, _milliseconds);
+        return time.ToString("hh:mm:ss.fff tt");
+    }
+
+    private int ValidateHour(int hour)
+    {
+        if (hour < 0 || hour > 23)
+        {
+            throw new ArgumentException($"The hour: {hour}, not is valid.");
+        }
+        return hour;
+    }
+    private int ValidateMinute(int minute) { if (minute < 0 || minute > 59) { throw new ArgumentException($"The minute: {minute}, not is valid."); } return minute; }
+    private int ValidateSeconds(int seconds) { if (seconds < 0 || seconds > 59) { throw new ArgumentException($"The seconds: {seconds}, not is valid."); } return seconds; }
+    private int ValidateMilliseconds(int milliseconds) { if (milliseconds < 0 || milliseconds > 999) { throw new ArgumentException($"The milliseconds: {milliseconds}, not is valid."); } return milliseconds; }
+
+    public int ToMilliseconds()
+    {
+        return _hour * 3600000 + _minute * 60000 + _seconds * 1000 + _milliseconds;
+    }
+
+    public int ToSeconds()
+    {
+        return _hour * 3600 + _minute * 60 + _seconds;
+    }
+
+    public int ToMinutes()
+    {
+        return _hour * 60 + _minute;
+    }
+
+
+
+
+
+
+}
