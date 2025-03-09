a97fc848430228e79f2a553afccd33abbe96656b
SergioVF89 <fonnegrasergio@gmail.com>
2025-03-09 08:02:31 -0500

    Actualización

--- POOConcepts/POOConcepts/Date.cs
+++ POOConcepts/POOConcepts/Date.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace POOConcepts;
+﻿namespace POOConcepts;
 
 public class Date
 {
     private int _day;
     private int _month;
     private int _year;
-
 
     public Date()
     {
@@ -23,49 +16,77 @@ public class Date
     public Date(int year, int month, int day)
     {
         _year = ValidateYear(year);
-        _month = ValidateMonth (month);
+        _month = ValidateMonth(month);
         _day = ValidateDay(day);
     }
 
-    public int Day { 
+    public int Day
+    {
         get => _day;
-        set => _day = value;       
+        set => _day = ValidateDay(value);
     }
 
     public int Month
     {
         get => _month;
-        set => _month = value;
+        set => _month = ValidateMonth(value);
     }
 
     public int Year
     {
         get => _year;
-        set => _year = value;
+        set => _year = ValidateYear(value);
     }
 
     public override string ToString()
     {
         return $"{_year:0000}/{_month:00}/{_day:00}";
     }
-    private int ValidateYear(int year) { if (year < 0) { throw new ArgumentException($"The year: {year}, not is valid."); } return year; }
 
+    private int ValidateYear(int year)
+    {
+        if (year < 0)
+        {
+            throw new ArgumentException($"The year: {year}, not is valid.");
+        }
+
+        return year;
+    }
 
     private int ValidateMonth(int month)
     {
-        if (month<1 || month > 12)
+        if (month < 1 || month > 12)
         {
-            throw new ArgumentException($"the month: : { month}, not is valid");
+            throw new ArgumentException($"The month: {month}, not is valid.");
         }
+
         return month;
     }
 
     private int ValidateDay(int day)
     {
-        if (day < 1) { throw new ArgumentException($"The day: {day}, not is valid."); }
-        if (day == 29 && _month == 2 && IsLeapYear(_year)) { return day; }
-        if ((day <= 28 && _month == 2) || (day <= 30 && (_month == 4 || _month == 6 || _month == 9 || _month == 11)) || (day <= 31 && (_month == 1 || _month == 3 || _month == 5 || _month == 7 || _month == 8 || _month == 10 || _month == 12))) { return day; }
+        if (day < 1)
+        {
+            throw new ArgumentException($"The day: {day}, not is valid.");
+        }
+
+        if (day == 29 && _month == 2 && IsLeapYear(_year))
+        {
+            return day;
+        }
+
+        if ((day <= 28 && _month == 2) ||
+            (day <= 30 && (_month == 4 || _month == 6 || _month == 9 || _month == 11)) ||
+            (day <= 31 && (_month == 1 || _month == 3 || _month == 5 || _month == 7 || _month == 8 || _month == 10 || _month == 12)))
+        {
+            return day;
+        }
+
         throw new ArgumentException($"The day: {day}, not is valid.");
     }
-    private bool IsLeapYear(int year) { return year % 400 == 0 || year % 4 == 0 && year % 100 != 0; }
+
+    private bool IsLeapYear(int year)
+    {
+        return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+    }
 }
--- POOConcepts/POOConcepts/Program.cs
+++ POOConcepts/POOConcepts/Program.cs
@@ -1,16 +1,39 @@
 ﻿using POOConcepts;
-try {
-    var date1 = new Date();
-    var date2 = new Date(1900, 12, 5);
-    var date3 = new Date(2020, 2, 28);
 
-    Console.WriteLine(date1);
-    Console.WriteLine(date2);
-    Console.WriteLine(date3);
+try
+{
+    var secretary = new SalaryEmployee(1, "Sandra", "Martinez", true, new Date(1981, 11, 14), new Date(2022, 12, 14), 2500000M);
+    var manager = new SalaryEmployee(2, "Fabio", "Ochoa", true, new Date(1971, 11, 14), new Date(2002, 12, 14), 11756324.15M);
+    var washer = new HourlyEmployee(3, "Jaime", "Betancur", true, new Date(1980, 12, 1), new Date(2020, 4, 20), 178, 10250.56M);
+    var salesPerson1 = new ComissionEmployee(4, "Monica", "Gil", true, new Date(1989, 7, 1), new Date(2020, 4, 20), 0.03F, 670000000M);
+    var salesPerson2 = new ComissionEmployee(5, "Henry", "Vasquez", true, new Date(1979, 1, 1), new Date(2020, 4, 20), 0.03F, 59000000M);
+    var salesPerson3 = new BaseComissionEmployee(6, "Patrica", "Rodriguez", true, new Date(1990, 2, 23), new Date(2025, 1, 10), 0.015F, 97000000M, 650000M);
+
+    var employees = new List<Employee>() { secretary, manager, washer, salesPerson1, salesPerson2, salesPerson3 };
+    var total = 0M;
+    Console.WriteLine("PAYROLL");
+    foreach (var employee in employees)
+    {
+        Console.WriteLine(employee);
+        Console.WriteLine("---------------------------------------------");
+        total += employee.GetValueToPay();
+    }
+
+    var invoioce1 = new Invoice(1, "Sillas", new Date(2025, 2, 27), 800000M, 6);
+    var invoioce2 = new Invoice(2, "Papel", new Date(2025, 2, 27), 56000M, 12);
+    var invoioce3 = new Invoice(3, "Portatil Dell", new Date(2025, 2, 27), 2567789M, 3);
+    var invoices = new List<Invoice>() { invoioce1, invoioce2, invoioce3 };
+
+    Console.WriteLine("EXPENSES");
+    foreach (var invoice in invoices)
+    {
+        Console.WriteLine(invoice);
+        Console.WriteLine("---------------------------------------------");
+        total += invoice.GetValueToPay();
+    }
+    Console.WriteLine($"TOTAL..................: {total,20:C2}");
 }
 catch (Exception ex)
 {
     Console.WriteLine(ex);
 }
-
-
