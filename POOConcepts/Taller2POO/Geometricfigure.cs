71900c9f3651a43fefed3d9895f639d3d0b6e176
SergioVF89 <fonnegrasergio@gmail.com>
2025-03-09 11:20:29 -0500

    Square

---
+++ POOConcepts/Taller2POO/Geometricfigure.cs
@@ -0,0 +1,17 @@
+﻿namespace Taller2POO;
+
+public abstract class Geometricfigure
+{
+    protected Geometricfigure(string name)
+    {
+        Name = name;
+    }
+    public string Name { get; set; } = null!;
+    public abstract double GetPerimeter();
+    public abstract double GetArea();
+
+    public override string ToString()
+    {
+        return $"{Name,-12} | => Area...... | {GetArea(),10:F5} | Perimeter: | {GetPerimeter(),10:F5} |";
+    }
+}
---
+++ POOConcepts/Taller2POO/Square.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+
+namespace Taller2POO;
+
+public class Square : Geometricfigure
+{
+    public Square(string v, double a) : base(v)
+    {
+        A = a;
+    }
+
+    private double _a;
+
+  public double A
+    {
+        get => _a;
+        set => _a = ValidateA(value);
+    }
+  
+
+    private double ValidateA(double side)
+    {
+        if (side <= 0)
+        {
+            throw new Exception($"The side: {side}, not is valid.");
+        }
+        return side;
+    }
+ 
+
+
+    public override double GetPerimeter()
+    {
+        return 4 * _a;
+    }
+
+    public override double GetArea()
+    {
+        return _a * _a;
+    }
+}
